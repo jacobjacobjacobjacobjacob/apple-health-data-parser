8045fd0bb86a5f94c36884a3bb534d87232bd4dd
jacobjacobjacobjacobjacob <jacob.schjetne@gmail.com>
2024-10-09 18:25:21 +0200

    main script for running all the cleaning functions

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -2,11 +2,17 @@
 import time
 import os
 from loguru import logger
-
 from src.constants.paths import CLEANED_DATA_DIRECTORY
-from src.constants.paths import PARSED_WORKOUT_DATA_PATH
-from src.cleaners.base_cleaner import BaseCleaner
+from src.constants.paths import (
+    PARSED_WORKOUT_DATA_PATH,
+    PARSED_HEALTH_DATA_PATH,
+    PARSED_ACTIVITY_DATA_PATH,
+    PARSED_SLEEP_DATA_PATH,
+)
 from src.cleaners.workout_cleaner import WorkoutCleaner
+from src.cleaners.health_cleaner import HealthCleaner
+from src.cleaners.activity_cleaner import ActivityCleaner
+from src.cleaners.sleep_cleaner import SleepCleaner
 
 
 def clean_all_data() -> dict:
@@ -15,18 +21,28 @@ def clean_all_data() -> dict:
 
     if not os.path.exists(CLEANED_DATA_DIRECTORY):
         os.makedirs(CLEANED_DATA_DIRECTORY)
-    
-    # Load parsed workout data
+
+    # # Workout data
     if os.path.exists(PARSED_WORKOUT_DATA_PATH):
-        logger.info(f"Cleaning workout data from: {PARSED_WORKOUT_DATA_PATH}")
         cleaner = WorkoutCleaner(PARSED_WORKOUT_DATA_PATH)
-        df = cleaner.clean_data()
+        cleaner.clean_data()
+
+    # Health data
+    if os.path.exists(PARSED_HEALTH_DATA_PATH):
+        cleaner = HealthCleaner(PARSED_HEALTH_DATA_PATH)
+        cleaner.clean_data()
+
+    # Activity data
+    if os.path.exists(PARSED_ACTIVITY_DATA_PATH):
+        cleaner = ActivityCleaner(PARSED_ACTIVITY_DATA_PATH)
+        cleaner.clean_data()
+
+    # Sleep data
+    if os.path.exists(PARSED_SLEEP_DATA_PATH):
+        cleaner = SleepCleaner(PARSED_SLEEP_DATA_PATH)
+        cleaner.clean_data()
 
 
     end_time = time.time()
     duration = end_time - start_time
-    logger.info(
-        f"Cleaning complete in {duration:.2f} seconds."
-    )
-    print(df.head())
-    return df
+    logger.info(f"Cleaning complete in {duration:.2f} seconds.")
