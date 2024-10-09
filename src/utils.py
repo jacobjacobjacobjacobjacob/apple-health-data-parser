a4c06ce65d2f1ccf5a141ebf5e7b2e5cc735a4e2
jacobjacobjacobjacobjacob <jacob.schjetne@gmail.com>
2024-10-09 08:12:17 +0200

    added save to csv function for tabular data

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,8 @@
 import json
 import os
 
+import pandas as pd
+
 def convert_to_json(data: list) -> str:
     return json.dumps(data, indent=4)
 
@@ -25,6 +27,25 @@ def save_json_to_file(data: list, directory: str, filename: str) -> None:
     # Save the JSON data to a file
     with open(file_path, 'w') as json_file:
         json.dump(data, json_file, indent=4)
+
+def save_csv_to_file(data: pd.DataFrame, directory: str, filename: str, overwrite: bool = True) -> None:
+    """
+    Save a DataFrame to a CSV file in the specified directory.
+    """
+
+    # Create directory if it does not exist
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # Define the full path for the output file
+    file_path = os.path.join(directory, filename)
+
+    # Check if the file already exists and handle overwrite
+    if os.path.isfile(file_path) and not overwrite:
+        return
+
+    # Save the DataFrame to a CSV file
+    data.to_csv(file_path, index=False)
 
 def check_if_file_exists(file_path: str) -> bool:
     """
