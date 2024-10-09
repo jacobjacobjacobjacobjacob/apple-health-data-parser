198f1791cc77091ecc794930320c9204700a27b5
jacobjacobjacobjacobjacob <jacob.schjetne@gmail.com>
2024-10-09 08:12:27 +0200

    main data cleaning module

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -1,12 +1,32 @@
 # src/data_cleaning.py
 import time
+import os
 from loguru import logger
-import pandas as pd
+
+from src.constants.paths import CLEANED_DATA_DIRECTORY
+from src.constants.paths import PARSED_WORKOUT_DATA_PATH
+from src.cleaners.base_cleaner import BaseCleaner
+from src.cleaners.workout_cleaner import WorkoutCleaner
+
 
 def clean_all_data() -> dict:
     logger.info("Cleaning all data.")
     start_time = time.time()
-    return None
-        
 
+    if not os.path.exists(CLEANED_DATA_DIRECTORY):
+        os.makedirs(CLEANED_DATA_DIRECTORY)
     
+    # Load parsed workout data
+    if os.path.exists(PARSED_WORKOUT_DATA_PATH):
+        logger.info(f"Cleaning workout data from: {PARSED_WORKOUT_DATA_PATH}")
+        cleaner = WorkoutCleaner(PARSED_WORKOUT_DATA_PATH)
+        df = cleaner.clean_data()
+
+
+    end_time = time.time()
+    duration = end_time - start_time
+    logger.info(
+        f"Cleaning complete in {duration:.2f} seconds."
+    )
+    print(df.head())
+    return df
