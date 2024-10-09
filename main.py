d53f045000da6c6a941f59e55a0a5bb8efb31baa
jacobjacobjacobjacobjacob <jacob.schjetne@gmail.com>
2024-10-09 18:25:30 +0200

    main entry point

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 # main.py
 from src.data_loading import unzip_export
 from src.data_parsing import parse_all_data
+from src.data_cleaning import clean_all_data
+
 
 if __name__ == "__main__":
+
     unzip_export()
     parse_all_data()
+    clean_all_data()
