bbcacbfa83f669b9b98ba31de5805e26a1d3acd6
jacobjacobjacobjacobjacob <jacob.schjetne@gmail.com>
2024-10-09 18:50:54 +0200

    refactoring

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from src.data_loading import unzip_export
 from src.data_parsing import parse_all_data
 from src.data_cleaning import clean_all_data
 
-
-if __name__ == "__main__":
-
+def main() -> None:
     unzip_export()
     parse_all_data()
     clean_all_data()
+
+if __name__ == "__main__":
+    main()
