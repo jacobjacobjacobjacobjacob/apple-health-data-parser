42fa5fc62b0643c241515fdeda90852f1e2e8547
jacobjacobjacobjacobjacob <jacob.schjetne@gmail.com>
2024-10-10 10:43:52 +0200

    formatting

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -22,17 +22,17 @@ def clean_all_data() -> dict:
     if not os.path.exists(CLEANED_DATA_DIRECTORY):
         os.makedirs(CLEANED_DATA_DIRECTORY)
 
-    # # Workout data
+    # Workout data
     if os.path.exists(PARSED_WORKOUT_DATA_PATH):
         cleaner = WorkoutCleaner(PARSED_WORKOUT_DATA_PATH)
         cleaner.clean_data()
 
-    # Health data
+    # # Health data
     if os.path.exists(PARSED_HEALTH_DATA_PATH):
         cleaner = HealthCleaner(PARSED_HEALTH_DATA_PATH)
         cleaner.clean_data()
 
-    # Activity data
+    # # Activity data
     if os.path.exists(PARSED_ACTIVITY_DATA_PATH):
         cleaner = ActivityCleaner(PARSED_ACTIVITY_DATA_PATH)
         cleaner.clean_data()
@@ -41,7 +41,6 @@ def clean_all_data() -> dict:
     if os.path.exists(PARSED_SLEEP_DATA_PATH):
         cleaner = SleepCleaner(PARSED_SLEEP_DATA_PATH)
         cleaner.clean_data()
-
 
     end_time = time.time()
     duration = end_time - start_time
