a33c13f59063bd874bec1922eb8c9bb369d7aca2
jacobjacobjacobjacobjacob <jacob.schjetne@gmail.com>
2024-10-12 08:57:45 +0200

    added constants for easier config

--- src/analysis/plots/plots.py
+++ src/analysis/plots/plots.py
@@ -6,75 +6,86 @@ from src.analysis.plots.plot_utils import (
     create_box_plot,
 )
 
-# Main function to run the plotting examples
+# Constants
+PLOT_WIDTH = 500
+PLOT_HEIGHT = 400
+SAVE_PLOT_AS_PNG = False
+SHOW_PLOT = True
+
+def print_available_dataframes(dataframes):
+    for dataframe_name in dataframes.items():
+        print(dataframe_name)
+
+def create_plots(df):
+    try:
+        create_histogram(
+            df,
+            x_axis="Resting Heartrate",
+            title="Histogram of Resting Heartrate",
+            show_plot=SHOW_PLOT,
+            save_plot_as_png=SAVE_PLOT_AS_PNG,
+            file_name="resting_heartrate_histogram.png",
+            width=PLOT_WIDTH,
+            height=PLOT_HEIGHT,
+            bargap=0,
+        )
+
+        create_line_plot(
+            df,
+            x_axis="date",
+            y_axis="Step Count",
+            show_plot=SHOW_PLOT,
+            save_plot_as_png=SAVE_PLOT_AS_PNG,
+            file_name="rolling_step_count.png",
+            rolling_average=True,
+            rolling_window_days=14,
+            y_label="Step Count",
+            x_label="Date",
+            width=900,
+            height=PLOT_HEIGHT,
+        )
+
+        create_histogram(
+            df,
+            x_axis="Sleep Hours",
+            title="Histogram of Sleep Hours",
+            color="#5F9EA0",
+            show_plot=SHOW_PLOT,
+            save_plot_as_png=SAVE_PLOT_AS_PNG,
+            nbins=20,
+            file_name="sleep_hours_histogram.png",
+            width=PLOT_WIDTH,
+            height=PLOT_HEIGHT,
+            bargap=0,
+            xaxis_title="Sleep Hours",
+        )
+
+        create_box_plot(
+            df,
+            x_axis="day_of_week",
+            y_axis="Sleep Hours",
+            title="Box Plot of Sleep Hours by Day of Week",
+            show_plot=SHOW_PLOT,
+            save_plot_as_png=SAVE_PLOT_AS_PNG,
+            file_name="sleep_by_day_of_week.png",
+            y_label="Sleep Hours",
+            x_label="Day of Week",
+            width=600,
+            height=PLOT_HEIGHT,
+        )
+    except Exception as e:
+        print(f"An error occurred while creating plots: {e}")
+
 if __name__ == "__main__":
-    # Load data
-    dataframes = load_dataframes()
-    df = get_merged_dataframe(dataframes)
+    try:
+        # Load data
+        dataframes = load_dataframes()
+        df = get_merged_dataframe(dataframes)
 
-    # Function to print available dataframes
-    def print_available_dataframes(dataframes):
-        for dataframe_name, df in dataframes.items():
-            print(dataframe_name)
+        # Print available dataframes
+        #print_available_dataframes(dataframes)
 
-    # Print available dataframes
-    # print_available_dataframes(dataframes)
-
-    create_histogram(
-        df,
-        x_axis="Resting Heartrate",
-        title="Histogram of Resting Heartrate",
-        show_plot=True,
-        save_plot_as_png=True,
-        file_name="resting_heartrate_histogram.png",
-        width=500,  # Plot width
-        height=400,  # Plot height
-        bargap=0,  # No gap between bars
-    )
-
-    # Create rolling Step Count line plot
-    create_line_plot(
-        df,
-        x_axis="date",
-        y_axis="Step Count",
-        show_plot=True,
-        save_plot_as_png=True,
-        file_name="rolling_step_count.png",
-        rolling_average=True,  # Enable rolling average
-        rolling_window_days=14,  # Set rolling window size to 14 days
-        y_label="Step Count",
-        x_label="Date",
-        width=900,  # Plot width
-        height=400,  # Plot height
-    )
-
-    # Create histogram for Sleep Hours
-    create_histogram(
-        df,
-        x_axis="Sleep Hours",
-        title="Histogram of Sleep Hours",
-        color="#5F9EA0",  # Custom color for bars
-        show_plot=True,
-        save_plot_as_png=True,
-        nbins=20,  # Number of bins for the histogram
-        file_name="sleep_hours_histogram.png",
-        width=500,  # Plot width
-        height=400,  # Plot height
-        bargap=0,  # No gap between bars
-        xaxis_title="Sleep Hours",
-    )
-
-    # Create box plot for Sleep Hours by day of the week
-    create_box_plot(
-        df,
-        x_axis="day_of_week",
-        y_axis="Sleep Hours",
-        title="Box Plot of Sleep Hours by Day of Week",
-        show_plot=True,
-        save_plot_as_png=True,
-        file_name="sleep_by_day_of_week.png",
-        y_label="Sleep Hours",
-        x_label="Day of Week",
-        width=600,  # Plot width
-        height=400,  # Plot height
-    )
+        # Create plots
+        create_plots(df)
+    except Exception as e:
+        print(f"An error occurred while loading data: {e}")
